6828a90616b5a1f0a708c6b1542965068c4ffd7b
jeanvitor06 <jeanvitor_06@hotmail.com>
2016-10-20 19:48:12 -0200

    add appset

--- lojadelivros/config/express.js
+++ lojadelivros/config/express.js
@@ -1,5 +1,6 @@
 var app = require('express')(); //chamando direto () === var x express()
-app.set('view engine','ejs'); //definir variaveis
+app.set('view engine','ejs'); //definir variaveis o default é views na raiz do projeto
+app.set('views','./app/views'); //caminho relativo ao local onde voce vai subir o servidor
 module.exports = function () {
     return app;
 }
