1fe41a38127c358fb7d9d0f08e33619635fd7296
jeanvitor06 <jeanvitor_06@hotmail.com>
2016-10-20 21:52:15 -0200

    removendo views da root

--- lojadelivros/main.js
+++ lojadelivros/main.js
@@ -2,5 +2,5 @@ var app = require('./config/express')(); // () mesma ideia do express
 var rotasProdutos = require('./app/routes/produtos')(app);
 
 app.listen(3000,function() {
-    console.log("servidor online");
+    console.log("servidor online!");
 });
