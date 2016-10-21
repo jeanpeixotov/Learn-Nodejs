eb296fff8e8d898f82505e253b7fd5f25c9dfbec
jeanvitor06 <jeanvitor_06@hotmail.com>
2016-10-21 21:58:32 -0200

    separate routes

--- lojadelivros/main.js
+++ lojadelivros/main.js
@@ -1,5 +1,5 @@
 var app = require('./config/express')(); // () mesma ideia do express
-var rotasProdutos = require('./app/routes/produtos')(app);
+//var rotasProdutos = require('./app/routes/produtos')(app);
 
 app.listen(3000,function() {
     console.log("servidor online!");
