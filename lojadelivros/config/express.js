25dae6d181d020396d445b2c553dc5b202eaf1b2
jeanvitor06 <jeanvitor_06@hotmail.com>
2016-10-21 22:06:24 -0200

    modify default express set, add bodyparser and load to loading arqs

--- lojadelivros/config/express.js
+++ lojadelivros/config/express.js
@@ -1,6 +1,19 @@
-var app = require('express')(); //chamando direto () === var x express()
-app.set('view engine','ejs'); //definir variaveis o default é views na raiz do projeto
-app.set('views','./app/views'); //caminho relativo ao local onde voce vai subir o servidor
+var express = require('express'); //chamando direto () === var x express()
+var load = require('express-load');
+var bodyParser = require('body-parser');
 module.exports = function () {
+    var app = express();
+
+    app.set('view engine','ejs'); //definir variaveis o default é views na raiz do projeto
+    app.set('views','./app/views'); // . é caminho relativo ao local onde voce vai subir o servidor
+
+    app.use(bodyParser.urlencoded({extended: true})); //extended para o bodyparser consegue interpretar o form corretamente(FK por exdmplo)
+    //funções que sao carregadas antes de chegar a requisicao middleware
+    //req->middlewarebodyparser -> middlewareautenticacao -> funcao que trata requisicao
+
+    //rotas,controllers etc loading
+    load('routes',{cwd: 'app'})
+        .then('infra')
+        .into(app);//tudo que for carregado fica aqui
     return app;
 }
