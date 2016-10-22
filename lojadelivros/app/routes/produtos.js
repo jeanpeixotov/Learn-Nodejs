73ccd645f156074aa1b4e04bd9e35182a5685b71
jeanvitor06 <jeanvitor_06@hotmail.com>
2016-10-21 22:00:03 -0200

    add new request salva and connection to ProdutosDAO

--- lojadelivros/app/routes/produtos.js
+++ lojadelivros/app/routes/produtos.js
@@ -1,18 +1,33 @@
+//var dbConnection = require('../infra/connectionFactory
 module.exports = function(app) {
-    app.get("/produtos",function(req, res) {
-        var mysql = require('mysql');
-        var connection = mysql.createConnection({
-            host: "localhost",
-            user: "root",
-            password : "12345678",
-            database: "lojadelivros"
-        });
+    //controllers
 
-        connection.query('select * from livros', function(err, results){
-            res.send(results);
+    app.get("/produtos",function(req, res) {
+
+        var connection = app.infra.connectionFactory();
+        var produtosDAO = new app.infra.ProdutosDAO(connection);
+        //new novo contexto de uso, ""'classe'""
+
+        produtosDAO.lista(function(err, results){
+            res.render('produtos/lista', {lista: results}); //returna json
+            //res.send(results); //json compreto do bd
         });
 
         connection.end();
+    });
 
+    app.get('/produtos/form', function(req,res) {
+        res.render('produtos/form');
+    });
+
+    app.post('/produtos/salva',function(req,res) {
+
+        var produto = req.body; //pega o insformações do form e grava na var em json ( graças ao bodyParser)
+        var connection = app.infra.connectionFactory();
+        var produtosDAO = new app.infra.ProdutosDAO(connection);
+
+        produtosDAO.salva(produto,function(err,results) {
+            res.render('produtos/lista');
+        });
     });
 }
