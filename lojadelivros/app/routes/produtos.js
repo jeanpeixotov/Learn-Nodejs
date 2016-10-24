e145a5d68f99c0e415805ffc843829c857026158
jeanvitor06 <jeanvitor_06@hotmail.com>
2016-10-24 10:36:11 -0200

    fix error reload createbook

--- lojadelivros/app/routes/produtos.js
+++ lojadelivros/app/routes/produtos.js
@@ -23,29 +23,30 @@ module.exports = function(app) {
     });
 
     app.get('/produtos/form', function(req,res) {
-        res.render('produtos/form',{errsVali:{},produto:{}});
+        res.render('produtos/form',{validationErrors:{},produto:{} });
     });
 
     app.post("/produtos",function(req,res) {
         var produto = req.body;//pega o insformações do form e grava na var em json ( graças ao bodyParser)
         //console.log(produto);
-        req.assert('titulo','Título obrigatório').notEmpty();
-        req.assert('preco','Formato inválido').isFloat();
-        var errs = req.validationErrors();
-        if(errs){
-            res.format({
-                html: function() {
-                    res.status(400).render('produtos/form',{errsVali:errs,produto:produto});
-                },
-                json: function () {
-                    res.status(400).json(errs);
-                }
-            });
-
-        }
-
         var connection = app.infra.connectionFactory();
         var produtosDAO = new app.infra.ProdutosDAO(connection);
+
+        req.assert('titulo','Título obrigatório').notEmpty();
+        req.assert('preco','Formato inválido').isFloat();
+        var errors = req.validationErrors();
+
+        if(errors){
+            res.format({
+                html: function(){
+                    res.status(400).render("produtos/form",{validationErrors:errors,produto:produto});
+                },
+                json: function(){
+                    res.status(400).send(errors);
+                }
+            });
+            return;
+        }
 
         produtosDAO.salva(produto,function(erros,resultado){
             res.redirect('/produtos');
