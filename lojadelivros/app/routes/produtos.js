fcbf5a909402baf8fdf1b8cfcc0b71810c87c88e
jeanvitor06 <jeanvitor_06@hotmail.com>
2016-10-24 08:56:24 -0200

    add res format and some asserts

--- lojadelivros/app/routes/produtos.js
+++ lojadelivros/app/routes/produtos.js
@@ -9,21 +9,40 @@ module.exports = function(app) {
         //new novo contexto de uso, ""'classe'""
 
         produtosDAO.lista(function(err, results){
-            res.render('produtos/lista', {lista: results}); //returna json
-            //res.send(results); //json compreto do bd
+            res.format({
+                html: function() {
+                    res.render('produtos/lista',{lista:results});
+                },
+                json: function () {
+                    res.json(results);
+                }
+            });
         });
 
         connection.end();
     });
 
     app.get('/produtos/form', function(req,res) {
-        res.render('produtos/form');
+        res.render('produtos/form',{errsVali:{},produto:{}});
     });
 
     app.post("/produtos",function(req,res) {
         var produto = req.body;//pega o insformações do form e grava na var em json ( graças ao bodyParser)
         //console.log(produto);
+        req.assert('titulo','Título obrigatório').notEmpty();
+        req.assert('preco','Formato inválido').isFloat();
+        var errs = req.validationErrors();
+        if(errs){
+            res.format({
+                html: function() {
+                    res.status(400).render('produtos/form',{errsVali:errs,produto:produto});
+                },
+                json: function () {
+                    res.status(400).json(errs);
+                }
+            });
 
+        }
 
         var connection = app.infra.connectionFactory();
         var produtosDAO = new app.infra.ProdutosDAO(connection);
