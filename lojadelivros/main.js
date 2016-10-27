2a7c8313d552d5d6fd7f4fd9e048828654b78ddc
jeanvitor06 <jeanvitor_06@hotmail.com>
2016-10-27 13:51:16 -0200

    add require app set socket

--- lojadelivros/main.js
+++ lojadelivros/main.js
@@ -1,6 +1,16 @@
 var app = require('./config/express')(); // () mesma ideia do express
 //var rotasProdutos = require('./app/routes/produtos')(app);
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-app.listen(3000,function() {
-    console.log("servidor online!");
+app.set('io',io);
+
+var porta = process.env.PORT || 3000;
+var server = http.listen(porta, function () {
+
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Server Online http://%s%s', host, port);
+
 });
