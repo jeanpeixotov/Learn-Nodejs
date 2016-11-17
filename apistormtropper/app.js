a5ec041a5ab1ceac889be949b55742590bd96fb3
Jean <jeanvitor_06@hotmail.com>
2016-11-17 10:35:09 -0200

    add error handling and cors

--- apistormtropper/app.js
+++ apistormtropper/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
-
+var cors = require('cors');
 //server config
 app.use(methodOverride('X-HTTP-Method'));
 app.use(methodOverride('X-HTTP-Method-Override'));
@@ -12,7 +12,24 @@ app.use(methodOverride('_method'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extented: true}));
 
+app.use(function (req, res, next) {
+   if (req.url === '/favicon.ico') {
+      res.writeHead(200, {'Content-type':'image/x-icon'});
+      res.end('');
+   } else {
+      next();
+   }
+});
 
+app.use(cors());
+//ou usar cors do npm para permitir acesso de todos a api
+// app.use(function(req,res,next){
+//    res.header('Access-Control-Allow-Origin','*');
+//    res.header('Access-Control-Allow-Headers','Origin, X-Request-With, Content-type, Accept');
+// });
+
+
+//routes
 app.get('/', function (req,res) {
    res.status(201);
    if(req.accepts('text')){
@@ -24,6 +41,19 @@ app.get('/', function (req,res) {
    }
 });
 
+//error handling
+app.use(function (req,res,next) {
+   var err = new Error('Not Found');
+   err.status = 404;
+   next(err);
+});
+
+app.use(function (err,req,res,next) {
+   console.log(err.stack);
+   res.status(err.status || 500).json({err:err.message});
+});
+
+//server listener
 var server = app.listen(3000, function () {
    var host = server.address().address;
    var port = server.address().port;
