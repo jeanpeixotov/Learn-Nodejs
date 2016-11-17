86533c3669f5bd035ab93eea0869fc2402f4427b
Jean <jeanvitor_06@hotmail.com>
2016-11-17 11:17:43 -0200

    add models stormtropper

---
+++ apistormtropper/models/StormtropperModel.js
@@ -0,0 +1,14 @@
+function StormtropperModel() {
+   StormtropperController.prototype.find = function (query ,callback);
+};
+
+StormtropperModel.prototype.create = function (data ,callback) {
+};
+
+StormtropperModel.prototype.update = function (_id ,data ,callback) {
+};
+
+StormtropperModel.prototype.remove = function (_id ,callback) {
+};
+
+module.exports = new StormtropperModel();
