69829ed7e7b2cb3997452b22eb02bd396504fc86
Jean <jeanvitor_06@hotmail.com>
2016-11-20 13:50:28 -0200

    routes finished

--- apistormtropper/routes/stormtroppers.js
+++ apistormtropper/routes/stormtroppers.js
@@ -1,9 +1,13 @@
 var express = require('express')
    ,router = express.Router();
 
-router.get('/', StormtropperController.getAll);
-router.get('/:_id', StormtropperController.getById);
-router.post('/', StormtropperController.create);
-router.update('/:_id', StormtropperController.update);
-router.delete('/:id', StormtropperController.remove);
+var mongo = require('../db/mongo');
+var StormtropperModel = require('../models/StormtropperModel')(mongo);
+var StormtropperController = require('../controllers/StormtropperController')(StormtropperModel);
+
+router.get('/', StormtropperController.getAll.bind(StormtropperController));
+router.get('/:_id', StormtropperController.getById.bind(StormtropperController));
+router.post('/', StormtropperController.create.bind(StormtropperController));
+router.put('/:_id', StormtropperController.update.bind(StormtropperController));
+router.delete('/:id', StormtropperController.remove.bind(StormtropperController));
 module.exports = router;
