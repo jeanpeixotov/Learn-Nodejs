3cc6bda8bb5434fa2103457b16526a3bb6c56f40
Jean <jeanvitor_06@hotmail.com>
2016-11-20 13:52:42 -0200

    controllers refactoring

--- apistormtropper/controllers/StormtropperController.js
+++ apistormtropper/controllers/StormtropperController.js
@@ -1,20 +1,59 @@
-function StormtropperController() {}
+var debug = require('debug')('apistormtropper:controller');
+function StormtropperController(StormtropperModel) {
+   this.model = StormtropperModel;
+}
 
 StormtropperController.prototype.getAll = function (req,res,next) {
-   res.send('get all stromtroppers');
+   this.model.find({}, function (err, data) {
+      if(err){
+         return next(err);
+      }
+      res.json(data);
+   });
 };
 
 StormtropperController.prototype.getById = function (req,res,next) {
-   res.send('get a specific stromtropper by id');
+   var _id = req.params._id;
+   this.model.findOne(_id, function (err, data) {
+      if(err){
+         return next(err);
+      }
+      if(!data){
+         var err = new Error('Not Found');
+         err.status = 404;
+         return next(err);
+      }
+      res.json(data);
+   });
 };
 StormtropperController.prototype.create = function (req,res,next) {
-   res.send('create a new stromtropper');
+   var body = req.body;
+   this.model.create(_id, function (err, data) {
+      if(err){
+         return next(err);
+      }
+      res.json(data);
+   });
 };
 StormtropperController.prototype.update = function (req,res,next) {
-   res.send('update a stromtropper');
+   var _id = req.params._id;
+   this.model.update(_id, function (err, data) {
+      if(err){
+         return next(err);
+      }
+      res.json(data);
+   });
 };
 StormtropperController.prototype.remove = function (req,res,next) {
-   res.send('remove a stromtropper');
+   var _id = req.params._id;
+   this.model.remove(_id, function (err, data) {
+      if(err){
+         return next(err);
+      }
+      res.json(data);
+   });
 };
 
-module.exports = new StormtropperController();
+module.exports = function (StormtropperModel) {
+   return new StormtropperController(StormtropperModel);
+};
