4367a87540185fc569496cf3be93ab11b2324e5f
Jean <jeanvitor_06@hotmail.com>
2016-11-23 20:28:56 -0200

    change require

--- apistormtropper/routes/stormtroppers.js
+++ apistormtropper/routes/stormtroppers.js
@@ -1,13 +1,13 @@
 var express = require('express')
    ,router = express.Router();
 
-var mongo = require('../db/mongo');
-var StormtropperModel = require('../models/StormtropperModel')(mongo);
+var mongoose = require('../db/mongoose');
+var StormtropperModel = require('../models/StormtropperModel')(mongoose);
 var StormtropperController = require('../controllers/StormtropperController')(StormtropperModel);
 
 router.get('/', StormtropperController.getAll.bind(StormtropperController));
 router.get('/:_id', StormtropperController.getById.bind(StormtropperController));
 router.post('/', StormtropperController.create.bind(StormtropperController));
 router.put('/:_id', StormtropperController.update.bind(StormtropperController));
-router.delete('/:id', StormtropperController.remove.bind(StormtropperController));
+router.delete('/:_id', StormtropperController.remove.bind(StormtropperController));
 module.exports = router;
