1024479b094f5d41435b80cef24b46dbd248ef59
Jean <jeanvitor_06@hotmail.com>
2016-11-23 20:31:57 -0200

    change callback to promises

--- apistormtropper/controllers/StormtropperController.js
+++ apistormtropper/controllers/StormtropperController.js
@@ -1,57 +1,60 @@
 var debug = require('debug')('apistormtropper:controller');
+var Promise = require('bluebird');
+
+var handleNotFound = function (data) {
+    if(!data){
+       var err = new Error('Not Found');
+       err.status = 404;
+       throw err;
+    }
+    return data;
+};
+
 function StormtropperController(StormtropperModel) {
-   this.model = StormtropperModel;
+   this.model = Promise.promisifyAll(StormtropperModel);
 }
 
 StormtropperController.prototype.getAll = function (req,res,next) {
-   this.model.find({}, function (err, data) {
-      if(err){
-         return next(err);
-      }
-      res.json(data);
-   });
+   this.model.findAsync({})
+        .then(function (data) {
+            res.json(data);
+        })
+        .catch(next);
 };
 
 StormtropperController.prototype.getById = function (req,res,next) {
    var _id = req.params._id;
-   this.model.findOne(_id, function (err, data) {
-      if(err){
-         return next(err);
-      }
-      if(!data){
-         var err = new Error('Not Found');
-         err.status = 404;
-         return next(err);
-      }
-      res.json(data);
-   });
+   this.model.findOneAsync(_id)
+        .then(handleNotFound)
+        .then(function (data) {
+            res.json(data);
+        })
+        .catch(next);
 };
 StormtropperController.prototype.create = function (req,res,next) {
    var body = req.body;
-   this.model.create(_id, function (err, data) {
-      if(err){
-         return next(err);
-      }
-      res.json(data);
-   });
+   this.model.createAsync(body)
+      .then(function (err,data) {
+          res.json(data)
+      })
+      .catch(next);
 };
 StormtropperController.prototype.update = function (req,res,next) {
    var _id = req.params._id;
-   this.model.update(_id, function (err, data) {
-      if(err){
-         return next(err);
-      }
-      res.json(data);
-   });
+   var body = req.body;
+   this.model.updateAsync(_id,body)
+      .then(function (err,data) {
+          res.json(data)
+      })
+      .catch(next);
 };
 StormtropperController.prototype.remove = function (req,res,next) {
    var _id = req.params._id;
-   this.model.remove(_id, function (err, data) {
-      if(err){
-         return next(err);
-      }
-      res.json(data);
-   });
+   this.model.removeAsync(_id) 
+      .then(function (err,data) {
+          res.json(data)
+      })
+      .catch(next);
 };
 
 module.exports = function (StormtropperModel) {
